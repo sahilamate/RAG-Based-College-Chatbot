33ace9c5ccfa588a790edcd0230bd6f1356946fd
sahilamate <sahilamate549@gmail.com>
2026-08-30 23:11:36 +0530

    Update API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://rag-based-college-chatbot-9um9.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://rag-based-college-chatbot-5.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
